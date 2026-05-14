2caf5df8c4ee53c9037704a90e8fb0960bb3c160
kalpeshgoswami <kalpeshgoswami298@gmail.com>
2026-05-14 13:45:43 +0530

    add DataType

---
+++ 03_dataType/DataType.js
@@ -0,0 +1,41 @@
+// ----- DataType -----
+
+// 1. string   ==> permeative
+// 2. Number
+// 3. boolean
+// 4. undefine
+// 5. null 
+// 6. symbol
+// 7. bigInt
+
+// 8. object   ==> non-permeative
+
+
+
+// ---- 1. string ----
+
+let string = "kalpesh";
+
+console.log("string:-",string);
+console.log(typeof string);
+
+// ---- 2. Number ----
+
+let Num = 123456;
+
+console.log("Number:-",Num);
+console.log(typeof Num);
+
+// ---- 3. boolean ----
+
+let result  = true;
+
+console.log("Boolean:-",result)
+console.log(typeof result);
+
+// ---- 4. undefine ----
+
+let a;
+
+console.log(a)
+console.log(typeof a);
