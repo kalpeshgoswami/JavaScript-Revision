eb350d3ca9fa32e356c315f36b5413d9029ea372
kalpeshgoswami <kalpeshgoswami298@gmail.com>
2026-06-03 13:24:18 +0530

    add arrray

---
+++ 09_array/01_array.js
@@ -0,0 +1,16 @@
+
+// ----- Array literal Syntax -----
+
+const games = ["GTA-V", "minecraft", "God of War", "Call of Duty"];
+
+console.log("array of games", games);
+
+console.log("type", typeof (games));
+
+// ----- Array with Constructor -----
+
+const play = new Array("Cricket", "Football", "Volleyball", "Tennis");
+
+console.log("array of games", play)
+
+console.log("type games array",typeof(play))
---
+++ 09_array/02_array.js
@@ -0,0 +1,21 @@
+
+
+// Create array 
+const games = ["Cricket", "Football", "Volleyball", "Tennis"];
+
+// Display the array
+console.log("Array of games:", games);
+
+//  first element
+console.log("First element:", games[0]);
+
+//  last element
+console.log("Last element:", games[games.length - 1]);
+
+// Modify element
+games[1] = "minecraft";
+console.log("Modified array:", games);
+
+// Add new element in last
+games.push("GTA-V");
+console.log("After push:", games);
